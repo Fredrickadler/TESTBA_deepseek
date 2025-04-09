367165de93800bf99d73937d17b6fb43b8571c1d
Fredrickadler <13fredrick2001@gmail.com>
2025-04-10 00:54:16 +0330

    Update sdk.js

--- farcaster/sdk.js
+++ farcaster/sdk.js
@@ -1,30 +1,8 @@
 import { sdk } from 'https://esm.sh/@farcaster/frame-sdk';
-import { getFrameConfig } from './frame-config.js';
 
-// تنظیمات اولیه
 sdk.configure({
     button: {
         wallet: '#connectWallet',
         mint: '#mintButton'
     }
-});
-
-// به‌روزرسانی خودکار فریم
-function updateFrame() {
-    const frameConfig = getFrameConfig();
-    const metaTag = document.querySelector('meta[name="fc:frame"]');
-    if (metaTag) {
-        metaTag.content = JSON.stringify(frameConfig);
-    }
-}
-
-// اولین بار که صفحه لود می‌شود
-updateFrame();
-
-// وقتی کاربر NFT را mint می‌کند
-sdk.on('mint-success', (data) => {
-    updateFrame({
-        image: data.nftImage,
-        buttonLabel: "🎉 View My NFT"
-    });
 });
