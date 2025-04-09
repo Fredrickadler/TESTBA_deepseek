86f20f506fa89468b4ea55479a824a9d7b58b7dc
Fredrickadler <13fredrick2001@gmail.com>
2025-04-10 01:25:18 +0330

    Update farme.js

--- pages/api/farme.js
+++ pages/api/farme.js
@@ -1,14 +1,8 @@
 export default function handler(req, res) {
-  const frame = {
+  res.status(200).json({
     version: "vNext",
     image: "https://i.imgur.com/muoYEIu.jpeg",
-    buttons: [{
-      label: "🪐 Mint Now",
-      action: "post"
-    }],
+    buttons: [{ label: "🪐 Mint Now", action: "post" }],
     postUrl: "https://testba-deepseek.vercel.app/api/frame"
-  };
-  
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).json(frame);
+  });
 }
