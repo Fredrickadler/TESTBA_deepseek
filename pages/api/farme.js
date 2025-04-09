a899a76b48285344ec7c76f39bdb3cbd091f9f79
Fredrickadler <13fredrick2001@gmail.com>
2025-04-10 01:41:48 +0330

    Delete pages/api directory

--- pages/api/farme.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    version: "vNext",
-    image: "https://i.imgur.com/muoYEIu.jpeg",
-    buttons: [{ label: "🪐 Mint Now", action: "post" }],
-    postUrl: "https://testba-deepseek.vercel.app/api/frame"
-  });
-}
